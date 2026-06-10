5b78189ea27b6db78b4ac4cc914b3dc3a4b0d294
kavindhanapal3758 <kavindhanapal02@gmail.com>
2026-06-10 08:27:20 +0530

    Add files via upload

---
+++ kavin lab 1.py
@@ -0,0 +1,128 @@
+'''#addition
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(a+b)
+
+#subraction
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(a-b)     
+
+#multipcation
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(a*b)
+
+#division
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(a/b)
+
+#modulus
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(a%b)
+
+#floor division
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(a**b)
+
+#exponentiation
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(a//b)
+
+#rectangle
+
+lenth=float(input("enter the length of the rectangle:"))
+width=float(input("enter the width of the rectangle:"))
+
+rect_area=lenth*width
+rect_perimeter=2*(lenth*width)
+
+print("rectangle area=",rect_area)
+print("rectangle perimeter=",rect_perimeter)
+
+#square
+
+side=float(input("\nEnter the side of the square:"))
+
+square_area=side*side
+square_perimeter=4*side
+
+print("square area=",square_area)
+print("square perimeter",square_perimeter)
+
+#circle
+
+radius=float(input("\nEnter the radius of the circle:"))
+
+circle_area=3.13*radius*radius
+circle_perimeter=2*3.14*radius
+
+print("circle area=",circle_area)
+print("circle circumference=",circle_perimeter)'''
+
+
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+c=float(input("enter third number:"))
+
+average=(a+b+c)/3
+print("average=",average)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
