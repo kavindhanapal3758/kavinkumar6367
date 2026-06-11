64cf138d49e488290644bad79fbb8feaaff2dd55
kavindhanapal3758 <kavindhanapal02@gmail.com>
2026-06-11 08:11:37 +0530

    Add files via upload

---
+++ kavin lab task.2.py
@@ -0,0 +1,178 @@
+'''a=int(input("Enter a:"))
+if a%2==0:
+    print("even")
+else:
+    print("odd")
+
+#2.
+a=int(input("enter a:"))
+if a>=18:
+    print("eligible")
+else:
+    print("not eligible")
+
+#3.
+a=int(input("Enter a:"))
+if a>=40:
+    print("pass")
+else:
+    print("fail")
+
+#4.
+a=int(input("Enter a:"))
+if a>0:
+    print("positive")
+else:
+    print("negative")
+
+#5.
+a=int(input("Enter a:"))
+if a>=90:
+    print("well done")
+elif a>=85:
+    print("good")
+elif a>=50:
+    print("should improve")
+else:
+    print("fail")
+
+#6.
+a=int(input("Enter a:"))
+if a/2==0 or a/5==0:
+    print("done")
+else:
+    print("not")
+
+#7.
+a=int(input("Enter a:"))
+if a-4==0 or a-9==0:
+    print("done")
+else:
+    print("not")
+
+#8.
+a=int(input("Enter a:"))
+if a+4==0 or a+9==0:
+    print("done")
+else:
+    print("not")
+
+#9.
+a=int(input("Enter a:"))
+if a*4==0 or a*9==0:
+    print("done")
+else:
+    print("not")
+
+#10.
+a=int(input("Enter a:"))
+b=int(input("enter b:"))
+c=int(input("Enter c:"))
+if a<=b:
+    print("greater")
+elif b<=c:
+    print("greater")
+else:
+    print("low")
+
+#11.
+u=int(input("enter the username:"))
+p=int(input("enter the password:"))
+if u== kavin0204:
+    print("enter password")
+        if p== 20954:
+           print("login success")
+       else:
+           print("invalid password")
+else:
+    print("invalid username")
+
+#12.
+print("red-stop")
+print("yellow-get ready")
+print("green-go")'''
+
+#13.
+r=int(input("enter red:"))
+if r==0:
+    print("stop")
+    y=int(input("enter yellow:"))
+    if y==0:
+        print("get ready")
+        g=int(input("enter green:"))
+        if g==0:
+            print("go")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
