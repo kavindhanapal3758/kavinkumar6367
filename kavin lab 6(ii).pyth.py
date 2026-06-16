8a03b26c9a06198aef4cfde8f72d8ea7ffa1d3e2
kavindhanapal3758 <kavindhanapal02@gmail.com>
2026-06-16 09:27:34 +0530

    Update kavin lab 6(ii).pyth.py

--- kavin lab 6(ii).pyth.py
+++ kavin lab 6(ii).pyth.py
@@ -1,37 +1,128 @@
-'''#list tasks
-#create a list of 5 student names and display it.
-'''a=['kavin','ruthran','abhi','samith']
+'''#1.
+#2.
+a=['kavin','ruthran','abhi','samith']
 for ch in a:
-    print(ch)'''
+    print(ch)
 
-#add a new element to the list using append().
+#3.
 b=[5,6,7,8]
 b.add(50)
 print(b)
 
-#insert an element at the 2nd position.
+#4.
 c=[6,7,8,9]
 print(c[2])
 
-#remove an element from the list.
+#5.
 d=[1,2,3,4,5,6,7]
 d.remove(5)
 print(d)
 
-#find the length of the list.
+#6.
 e=[6,5,7,8,]
  e.len()
  print(e)
 
-#reverse the list
+#7.
 f=[20,8,9,4]
 f.reverse()
 print(f)
 
-#find the largest number in a list.
+#8.
 g=[1,2,3,4,50,90]
+g.max()
+print(g)
 
+#9.
+h=[7,8,6,9,5,1,6]
+h.count(4)
+print(h)
 
+#12.
+#13.
+i=(1,2,3,4,5)
+print(i)
+
+#14.
+j=(6,7,8,9)
+print(len(j))
+
+#16.
+k=(1,2,3,4,5,6,7,8,9)
+print(k.index(9))
+
+#17.
+l=(9,4,6,2,8)
+print(l.index(2))
+
+#19.
+m=(2,4,6,8,10,19,12,14,16)
+print(max(m))
+print(min(m))
+
+#23.
+#24.
+n=(2,4,6,8,10)
+print(n)
+
+#25.
+o=(1,4,7,2,5,8)
+o.add(10)
+print(o)
+
+#26.
+p=(1,3,5,7,9)
+p.remove(9)
+print(p)
+
+#27.
+q=(2,4,6,8,10)
+q.discare(6)
+print(q)
+
+#28.
+r=(1,3,5,7,9)
+s=(2,4,6,8,10)
+print(r.union(s))
+
+#29.
+u={1,7,4,9}
+v={3,2,6,7}
+print{u.intersection(v)}
+
+#30.
+w={1,3,5,7,9}
+x={3,2,5,6}
+print(w.difference(x))
+
+#31.
+z={1,2,4,45}
+a={2,4,5,9,67}
+print(z.issubset(a))
+
+#32.
+b={1,3,5,7}
+c={2,4,6,8}
+print(b.issupperset(c))
+
+#38.
+d=(2,4,6,8,10)
+e=(1,3,5,7,9)
+print(d.intersection(e))
+
+#39.
+
+#list
+f=[kavin,ruthran,abhi,samith]
+print(f)
+
+#tuple
+i=(kavin,ruthran,abhi,samith)
+print(i)
+
+#set
+j={kavin,ruthran,abhi,samith}
+print(j)
  
 
 
