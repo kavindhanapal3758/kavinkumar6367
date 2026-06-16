03640ca9b0949e0934662eec1e184d8054031b40
kavindhanapal3758 <kavindhanapal02@gmail.com>
2026-06-16 07:50:34 +0530

    Add files via upload

---
+++ kavin lab 6(ii).pyth.py
@@ -0,0 +1,38 @@
+'''#list tasks
+#create a list of 5 student names and display it.
+'''a=['kavin','ruthran','abhi','samith']
+for ch in a:
+    print(ch)'''
+
+#add a new element to the list using append().
+b=[5,6,7,8]
+b.add(50)
+print(b)
+
+#insert an element at the 2nd position.
+c=[6,7,8,9]
+print(c[2])
+
+#remove an element from the list.
+d=[1,2,3,4,5,6,7]
+d.remove(5)
+print(d)
+
+#find the length of the list.
+e=[6,5,7,8,]
+ e.len()
+ print(e)
+
+#reverse the list
+f=[20,8,9,4]
+f.reverse()
+print(f)
+
+#find the largest number in a list.
+g=[1,2,3,4,50,90]
+
+
+ 
+
+
+
