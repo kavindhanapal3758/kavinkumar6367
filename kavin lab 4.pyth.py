e18e44563a12cf8c91c99046f99c4590eaafd16e
kavindhanapal3758 <kavindhanapal02@gmail.com>
2026-06-19 16:57:06 +0530

    Add files via upload

---
+++ kavin lab 4.pyth.py
@@ -0,0 +1,122 @@
+'''#1.
+i=1
+while i<=10:
+    print(i)
+    i+=1
+
+#2.
+i=10
+while i>=1:
+    print(i)
+    i-=1
+
+#3.
+i=2
+while i==(2*40):
+    print(i)
+    i+=2
+
+#4.
+n=int(input("Enter a:"))
+i=1
+s=0
+while i>=n:
+    s=s+i
+    i=i+1
+    print(s)
+
+#5.
+n=int(input("Enter n:"))
+f=1
+i=1
+while i>=n:
+    f=i*1
+    print("factorial:",f)
+
+#6.
+a=692
+b=0
+t=a
+while t<0:
+    a=t%10
+    b=(a*10)+10
+    t=d//10
+    print(b)
+    if a==b:
+        print("palindrome")
+    else:
+        print("not a palindrone")
+
+#7.
+a=1035
+b=0
+c=1
+t=a
+while t>0:
+    a=t%10
+    b=b+a
+    c=c*a
+    t=t//10
+print('c:',c)
+print('b:',b)
+if b==c:
+    print("spy number")
+else:
+    print("not")
+
+#8.
+a=3758
+b=0
+c=1
+t=a
+while t>0:
+    a=t%10
+    b=b=d
+    c=c*d
+print('c:',c)
+print('b:',b)
+if b==c:
+    print("spy numbers")
+else:
+    print("not a spy number")
+
+#9.
+i=1
+while i<=10:
+    print(i)
+    i+=1
+
+#10.
+a=int(input("Enter a:"))
+i=1
+while i<=20:
+    print(i)
+    i+=1
+if i%2==0:
+    print("even")
+else:
+    print("false")'''
+
+#11.
+a=int(input("Enter a:"))
+i=1
+while i<=20:
+    print(i)
+    i+=1
+if i%2!=0:
+    print("odd")
+else:
+    print("even")
+    
+
+    
+    
+    
+    
+    
+
+
+
+
+
+
